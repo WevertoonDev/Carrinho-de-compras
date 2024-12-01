e120e6dcd727624f1b10e8223c837713860c1584
weverton alves <wevertonm22@iclou.com>
2024-11-30 22:50:49 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function adicionar () {
+    let nomeProduto =  document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value)
+    alert(quantidade.value)
+}
+function limpar () {
+
+}
