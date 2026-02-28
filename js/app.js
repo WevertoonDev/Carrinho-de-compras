fdf8b924f1817260b46393968707c65571391dae
Weverton Martins <wevertonm22@icloud.com>
2026-02-28 09:56:51 -0300

    Recuperando produtos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-function adicionar (id) {
+function adicionar () {
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+    //recuperar valores nome do produto, quantidade e valor
+    //calcular preço ,o nosso subtotal
+    //adicionar no carrinho
+    //atualizar o valor total 
     
 }
+
+function limpar() {}
